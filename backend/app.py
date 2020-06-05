7cefb7bf82340fe1d8f34e1704c4c43ef0ccd188
Jacopo Farina <jacopo.farina@flixbus.com>
2020-06-05 17:07:36 +0200

    Add stub of endpoint to draw cards

--- backend/app.py
+++ backend/app.py
@@ -1,8 +1,10 @@
 import logging
 from pathlib import Path
+from typing import List
 
 from fastapi import FastAPI
 from fastapi.staticfiles import StaticFiles
+from pydantic import BaseModel
 from starlette.responses import RedirectResponse
 
 from backend.dbutil import get_conn, attach_db_cycle
@@ -41,3 +43,45 @@ async def current_time():
             """)
         return now['now_ts']
 
+
+class QuizRequest(BaseModel):
+    source_langs: List[str]
+    target_lang: str
+
+
+@app.post("/draw_cards")
+async def draw_cards(qr: QuizRequest):
+    """Return a bunch of random cards."""
+    # fake constant user id to simplify multi-user later
+    current_user = 1
+    # TODO would be nice to move queries to .sql files referred by name
+    # and have a concise for that
+    async with get_conn() as conn:
+        cards = await conn.fetch(
+            """
+            SELECT
+                fl.name     AS from_language,
+                tl.name     AS to_language,
+                c.from_id   AS from_id,
+                c.to_id     AS to_id,
+                c.from_txt  AS from_txt,
+                c.to_tokens AS to_tokens
+            FROM
+                card c
+                    JOIN language fl
+                        ON fl.id = c.from_lang
+                    JOIN language tl
+                        ON tl.id = c.to_lang
+                    LEFT JOIN card_user_state cus
+                            ON c.from_id = cus.from_id AND c.to_id = cus.to_id
+                                AND cus.account_id = $3
+            WHERE
+                fl.iso693_3 = ANY($2)
+            AND tl.iso693_3 = $1
+            AND cus.account_id IS NULL
+            LIMIT 10
+            """,
+            qr.target_lang,
+            qr.source_langs,
+            current_user)
+        return cards
--- scripts/populate_db/schema.sql
+++ scripts/populate_db/schema.sql
@@ -1,18 +1,34 @@
-CREATE TABLE languages (
+CREATE TABLE language (
     id       SMALLINT PRIMARY KEY,
     iso693_3 TEXT NOT NULL,
     name     TEXT
 );
 
-CREATE TABLE cards (
-    from_lang    SMALLINT NOT NULL REFERENCES languages,
-    to_lang      SMALLINT NOT NULL REFERENCES languages,
+CREATE TABLE card (
+    from_lang    SMALLINT NOT NULL REFERENCES language,
+    to_lang      SMALLINT NOT NULL REFERENCES language,
     from_id      INTEGER  NOT NULL,
     to_id        INTEGER  NOT NULL,
     from_txt     TEXT     NOT NULL,
     original_txt TEXT     NOT NULL,
-    to_tokens    TEXT[]   NOT NULL
+    to_tokens    TEXT[]   NOT NULL,
+    PRIMARY KEY (from_id, to_id)
 );
 
-CREATE INDEX cards_from_lang_to_lang_index
-    ON cards(from_lang, to_lang);
+CREATE INDEX card_from_lang_to_lang_index
+    ON card(from_lang, to_lang);
+
+CREATE TABLE account (
+    id INTEGER PRIMARY KEY
+);
+
+CREATE TABLE card_user_state(
+    from_id    INTEGER                  NOT NULL,
+    to_id      INTEGER                  NOT NULL,
+    account_id INTEGER                  NOT NULL,
+    last_seen  TIMESTAMP WITH TIME ZONE NOT NULL,
+    FOREIGN KEY (from_id, to_id) REFERENCES card(from_id, to_id),
+    FOREIGN KEY (account_id) REFERENCES account,
+    PRIMARY KEY (from_id, to_id, account_id)
+);
+
