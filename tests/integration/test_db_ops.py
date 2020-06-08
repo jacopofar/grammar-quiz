836c32107186e77c3050e5993ec018a96b27748b
Jacopo Farina <jacopo.farina@flixbus.com>
2020-06-08 09:16:56 +0200

    Add test for DB ops

---
+++ tests/integration/test_db_ops.py
@@ -0,0 +1,105 @@
+from fastapi.testclient import TestClient
+import pytest
+
+from backend.app import app
+
+
+@pytest.fixture
+def client():
+    with TestClient(app) as client:
+        yield client
+
+
+def test_get_languages(client):
+    languages = client.get("/languages").json()
+    assert len(languages) > 0
+    assert 'iso693_3' in languages[0]
+    # the codes are always 3 letters
+    assert len(languages[0]['iso693_3']) == 3
+    assert 'name' in languages[0]
+
+
+def test_draw_cards(client):
+    response = client.post(
+        "/draw_cards",
+        json=dict(
+            target_lang='deu',
+            source_langs=['eng', 'jap'],
+        ))
+    assert len(response.json()) == 10
+
+
+def test_answer_then_redraw(client):
+    initial_cards = client.post(
+        "/draw_cards",
+        json=dict(
+            target_lang='deu',
+            source_langs=['eng', 'jap'],
+        ))
+    second_draw = client.post(
+        "/draw_cards",
+        json=dict(
+            target_lang='deu',
+            source_langs=['eng', 'jap'],
+        ))
+    # drawing the cards is not affecting their availability
+    assert len(initial_cards.json()) > 1
+    assert len(initial_cards.json()) == len(second_draw.json())
+
+    card1 = second_draw.json()[0]
+    card2 = second_draw.json()[1]
+
+    # send a correct answer
+    ok_response = client.post(
+        "/register_answer",
+        json=dict(
+            from_id=card1['from_id'],
+            to_id=card1['to_id'],
+            expected_answers=['SOME', 'token'],
+            given_answers=['some', 'token'],
+            correct=True,
+        ))
+    assert ok_response.json() == 'OK'
+    # send a wrong answer
+    ok_response = client.post(
+        "/register_answer",
+        json=dict(
+            from_id=card2['from_id'],
+            to_id=card2['to_id'],
+            expected_answers=['another', 'token'],
+            given_answers=['wrong', 'stuff'],
+            correct=False,
+        ))
+    assert ok_response.json() == 'OK'
+    draw_after = client.post(
+        "/draw_cards",
+        json=dict(
+            target_lang='deu',
+            source_langs=['eng', 'jap'],
+        ))
+    cards = [
+        (card['from_id'], card['to_id'])
+        for card in draw_after.json()
+    ]
+    # the answers we already sent are not in the new selection
+    assert (card1['from_id'], card1['to_id']) not in cards
+    assert (card2['from_id'], card2['to_id']) not in cards
+# # received
+# from_language
+# 	"Italian"
+# to_language
+# 	"English"
+# from_id
+# 	4386
+# to_id
+# 	1393
+# from_txt
+# 	"Mi piacerebbe tanto andare in Giappone."
+# to_tokens
+# 	[ "I", "{{c1::wish}}", "I", "could", "go", "to", "Japan." ]
+# # to send
+# from_id: int
+#     to_id: int
+#     expected_answers: List[str]
+#     given_answers:  List[str]
+#     correct: bool
