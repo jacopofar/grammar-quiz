54d0d850c60c59804aa736b024442a910590bc47
Jacopo Farina <jacopo.farina@flixbus.com>
2020-06-16 18:56:41 +0200

    Add minimal authentication mechanism

--- backend/config.py
+++ backend/config.py
@@ -5,3 +5,8 @@ from pydantic import (
 
 class Config(BaseSettings):
     pg_conn_str: PostgresDsn = Field(..., env='PG_CONN_STR')
+    sso_google_client_id: str = Field(..., env='SSO_GOOGLE_CLIENT_ID')
+    sso_google_secret: str = Field(..., env='SSO_GOOGLE_SECRET')
+    secret_session_key: str = Field(..., env='SECRET_SESSION_KEY')
+
+
