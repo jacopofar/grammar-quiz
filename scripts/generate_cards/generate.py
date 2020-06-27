6a67a83c9b0f0b844ae26b58ab55ac4a18a7fbdd
Jacopo Farina <jacopo.farina@flixbus.com>
2020-06-28 00:17:03 +0200

    Use lemmas to generate cards containing base forms when possible

--- scripts/generate_cards/generate.py
+++ scripts/generate_cards/generate.py
@@ -4,6 +4,7 @@ from csv import reader
 import json
 from random import Random
 from sys import maxunicode
+from typing import Dict, Tuple, Set
 from unicodedata import category
 
 import icu
@@ -21,6 +22,12 @@ PUNCT_TRANSL = dict.fromkeys(
 
 # how likely is to add an extra cloze
 ANOTHER_CLOZE_FACTOR = 2
+
+# how likely to not insert a base form when possible but hide the cloze completely
+# if a dictionary form was always shown when available, the user would learn its
+# presence as a fact marking the type of word, which invalidates the efficacy of
+# cloze deletion. So sometimes they are hidden nonetheless
+HIDE_BASE_FORM_FACTOR = 2
 
 # how many clozes per sentence, it never goes above
 MAX_CLOZES = 3
@@ -70,18 +77,49 @@ def tokenize(text: str, lang: str):
     return text.split()
 
 
-def main_multi(sentence_file: str, link_file: str):
-    """Produce the cloze deletion cards.
+def get_unambiguous_roots(base_forms_file: str) -> Dict[str, Dict[str, str]]:
+    """Get the list of words repleaceable with an unambiguous lemma.
 
-    This will produce them for all the language pairs!
+    This is a dictionary having the ISO 3-letter code of each language as
+    a key, and as values dictionaries mapping words with their dictionary
+    form. When a word has multiple possible senses or base forms, it's
+    omitted.
+    Since this data is generated from en.wiktionary, some languages are
+    missing and some have very little data.
+    """
+    ret = {}
+    with open(base_forms_file) as f:
+        for line in f:
+            form = json.loads(line)
+            lang = form['lang']
+            if lang not in ret:
+                ret[lang] = {}
+            if len(form['base_forms']) == 1:
+                ret[lang][form['word']] = form['base_forms'][0]
+    return ret
+
+
+def read_sentences(sentence_file: str) -> Tuple[Dict[int, Tuple[str, str]], Dict[str, Set[str]]]:
+    """Read and process a sentence file.
+
+    Paramenters
+    -----------
+    sentence_file: str
+        name or path of a sentence file, usually sentences.csv from Tatoeba
+
+    Returns
+    -------
+    Tuple[Dict[int, Tuple[str, str]], Dict[str, Set[str]]]
+
+    A tuple with two elements:
+    * a dictionary mapping a sentence ID with its language code and content
+    * a dictionary mapping each language with a list of most common words
+
     """
     sents = reader(open(sentence_file), delimiter='\t')
-    links = reader(open(link_file), delimiter='\t')
-
     id_sents = {}  # id -> (lang, text)
     langs = set()  # set of seen lanuages
     word_counters = {}  # lang -> Counter
-    print(f'Processing files {sentence_file}, {link_file}...')
     for idx, [_id, lang, text] in enumerate(sents):
         if idx % 200_000 == 0:
             print(f'Read {idx} rows from the sentences CSV so far')
@@ -98,7 +136,6 @@ def main_multi(sentence_file: str, link_file: str):
         )
         langs.add(lang)
     print(f'Imported {len(id_sents)} sentences')
-
     most_commons = {}
     for lng in langs:
         # Note that in here there is an empty string,
@@ -109,7 +146,20 @@ def main_multi(sentence_file: str, link_file: str):
         most_commons[lng] = set(
             w for w, _ in word_counters[lng].most_common(WORD_MIN_RANK)
             )
-    del word_counters
+    return id_sents, most_commons
+
+
+def main_multi(sentence_file: str, link_file: str, base_forms_file: str):
+    """Produce the cloze deletion cards.
+
+    This will produce them for all the language pairs!
+    """
+    print(f'Processing files {sentence_file}, {link_file}, {base_forms_file}...')
+
+    links = reader(open(link_file), delimiter='\t')
+
+    id_sents, most_commons = read_sentences(sentence_file)
+    base_forms = get_unambiguous_roots(base_forms_file)
 
     pairs = []
     for idx, [from_id, to_id] in enumerate(links):
@@ -163,6 +213,7 @@ def main_multi(sentence_file: str, link_file: str):
             # no cloze of a cloze
             if tokens[to_replace_idx].startswith('{{'):
                 continue
+            norm_token = normalized(tokens[to_replace_idx])
             # do not put cloze after empty cloze or space
             if (
                     to_replace_idx > 0
@@ -172,12 +223,26 @@ def main_multi(sentence_file: str, link_file: str):
                         )
                     ):
                 continue
+            # is it an unambiguous inflected form?
+            if norm_token in base_forms.get(to_lang, {}):
+                if r.randint(1, HIDE_BASE_FORM_FACTOR) != 1:
+                    tokens[to_replace_idx] = ''.join([
+                        '{{c',
+                        str(cloze_idx),
+                        ':',
+                        base_forms[to_lang][norm_token],
+                        ':',
+                        tokens[to_replace_idx],
+                        '}}'
+                    ])
+                    cloze_idx += 1
+                    continue
+
             # only the most common words
-            if (normalized(tokens[to_replace_idx])
-                    not in most_commons[to_lang]):
+            if (norm_token not in most_commons[to_lang]):
                 continue
             if tokens[to_replace_idx] == ' ':
-                if r.randint(0, TOLERATE_SPACE_FACTOR) > 0:
+                if r.randint(1, TOLERATE_SPACE_FACTOR) != 1:
                     continue
                 # if the next element is a cloze, do not replace or would
                 # be ambiguous for the user
@@ -198,7 +263,7 @@ def main_multi(sentence_file: str, link_file: str):
             if cloze_idx > MAX_CLOZES:
                 continue
 
-            if r.randint(0, EMPTY_CLOZE_FACTOR) == 0:
+            if r.randint(1, EMPTY_CLOZE_FACTOR) == 1:
                 to_insert_idx = r.randint(0, len(tokens) - 1)
                 # do it only if there'not a cloze on the right
                 # otherwise the user has no way to know this is a fake one
@@ -242,5 +307,7 @@ if __name__ == '__main__':
     parser = argparse.ArgumentParser()
     parser.add_argument('sentences', help='the sentence CSV file', type=str)
     parser.add_argument('links', help='the links CSV file', type=str)
+    parser.add_argument('base_forms', help='the base forms JSONL file', type=str)
+
     args = parser.parse_args()
-    main_multi(args.sentences, args.links)
+    main_multi(args.sentences, args.links, args.base_forms)
